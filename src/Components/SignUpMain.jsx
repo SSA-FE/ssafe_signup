cd782a4d42569e42b6a483718dc728b9b607dda3
ChaMin <kusoft1997@gmail.com>
2024-02-27 01:23:09 +0900

    feature) 이메일, 비밀번호,체크 유효성 검사 기능 구현

--- src/Components/SignUpMain.jsx
+++ src/Components/SignUpMain.jsx
@@ -1,7 +1,8 @@
-import React, { useState } from 'react';
+import React, { useState} from 'react';
 import styled from "styled-components";
 import SignUpTopBar from './SignUpTopBar';
-const SignUpInputStyle=styled.div`
+import { useNavigate } from 'react-router-dom';
+const SignUpInputStyle = styled.div`
 display:flex;
 flex-direction:column;
 align-items:center;
@@ -32,7 +33,7 @@ input:focus{
   outline:none;
 }
 `
-const SignUpMainStyle=styled.div`
+const SignUpMainStyle = styled.div`
 .MainContainer{
   display:flex;
   flex-direction:column;
@@ -56,50 +57,93 @@ h1{
   font-size:20px;
   font-weight:700;
 }
+
 `
 
 const SignUpMain = () => {
-    const [email, setEmail] = useState('');
-    const [emailError, setEmailError] = useState(false); 
-    
-    const checkEmail = () => {
-      if (!/^[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/i.test(email)) {
-        setEmailError(true);
-      } else {
-        setEmailError(false);
-      }
-    
+  const nav=useNavigate();
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [passwordCheck, setPasswordCheck] = useState('');
+  const [emailError, setEmailError] = useState(false);
+  const [passwordError, setPasswordError] = useState(false);
+  const [passwordCheckError, setPasswordCheckError] = useState(false);
+ 
+  const checkEmail = () => {
+    const emailRegex = /^[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/i
+    if (!emailRegex.test(email)) {
+      setEmailError(true);
+    } else {
+      setEmailError(false);
     }
-    return (    
-        <>
-          <SignUpTopBar/>
-          <SignUpMainStyle>
-            <div className='MainContainer'>
-            <h1 className='MainText'>
-              더 쉬운 폼을 위한 시작<br/>
-              폼나는 싸패
-            </h1>
-            <SignUpInputStyle error={emailError}>
+    console.log(emailError || passwordError || passwordCheckError)
+ 
+
+    
+  }
+  const setSignUpBtn=()=>{
+    
+  }
+  const checkPassword = () => {
+    const passwordRegex = /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,16}$/;
+    if (!passwordRegex.test(password)) {
+      setPasswordError(true);
+    }
+    else {
+      setPasswordError(false);
+    }
+  }
+  const checkPasswordCheck = () => {
+    if (passwordCheck != password) {
+      setPasswordCheckError(true);
+    }
+    else {
+      setPasswordCheckError(false);
+    }
+  }
+  const handleSignUp = () => {
+    checkEmail();
+    checkPassword();
+    checkPasswordCheck();
+
+    if (!emailError && !passwordError && !passwordCheckError&&email!=''&&password!=''&&passwordCheck!='') {
+      alert(`Email: ${email}, Password: ${password}`);
+      nav('/');
+    } else {
+      console.log('회원가입 버튼 클릭: 유효성 검사 실패');
+    }
+
+  }
+  return (
+    <>
+      <SignUpTopBar />
+      <SignUpMainStyle>
+        <div className='MainContainer'>
+          <h1 className='MainText'>
+            더 쉬운 폼을 위한 시작<br />
+            폼나는 싸패
+          </h1>
+          <SignUpInputStyle error={emailError}>
             <b>이메일</b>
-            <input className='MainInputEmail' placeholder='이메일을 입력하세요' value={email} onChange={(e)=>setEmail(e.target.value)}onBlur={checkEmail}></input>
+            <input className='MainInputEmail' placeholder='이메일을 입력하세요' value={email} onChange={(e) => setEmail(e.target.value)} onBlur={checkEmail}></input>
             <span className='MainInputEmailErr'>유효하지않은 이메일 입니다.</span>
-            </SignUpInputStyle>
-            <SignUpInputStyle>
+          </SignUpInputStyle>
+          <SignUpInputStyle error={passwordError}>
             <b>비밀번호</b>
-            <input className='MainInputPw' placeholder='비밀번호를 입력하세요'></input>
+            <input className='MainInputPw' placeholder='비밀번호를 입력하세요' value={password} onChange={(e) => setPassword(e.target.value)} onBlur={checkPassword}></input>
             <span className='MainInputPwErr'>유효하지않은 비밀번호 입니다.</span>
-            </SignUpInputStyle>
-            <SignUpInputStyle>
+          </SignUpInputStyle>
+          <SignUpInputStyle error={passwordCheckError}>
             <b>비밀번호 확인</b>
-            <input className='MainInputPwCheck' placeholder='비밀번호를 다시 입력하세요'></input>
+            <input className='MainInputPwCheck' placeholder='비밀번호를 다시 입력하세요' value={passwordCheck} onChange={(e) => setPasswordCheck(e.target.value)} onBlur={checkPasswordCheck}></input>
             <span className='MainInputPwCheckErr'>비밀번호가 일치하지 않습니다.</span>
-            </SignUpInputStyle>
-            <button className='MainSignUpBtn'>회원가입</button>
-            </div>
-        
-          </SignUpMainStyle>
-        </>
-    );
+          </SignUpInputStyle>
+          <button className='MainSignUpBtn' onClick={handleSignUp}>회원가입</button>
+        </div>
+
+      </SignUpMainStyle>
+    </>
+  );
 };
 
 export default SignUpMain;
