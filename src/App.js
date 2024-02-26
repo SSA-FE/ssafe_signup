16a717b684bb1171cd37d2db1993a1add52f1148
woongveloper <woongveloper98@gmail.com>
2024-02-26 17:53:57 +0900

    Routing Try

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import { BrowserRouter, Routes, Route, Link} from "react-router-dom";
+import SignUp from './SignUp';
+//  새로고침 없이 이동 -> Link 사용
+
+
+// https://www.daleseo.com/react-router-basic/
+// 이거 참고해서 다시 해보쟈
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+        <header>
+          <h1>HomePage</h1>
+          {/* <button onClick={onClickSignUp}>회원가입</button> */}
+            <Link to="/">
+              <button>Home</button>
+            </Link>
+            <Link to="/SignUp">
+              <button>회원가입</button>
+            </Link>
+        </header>
+        <main>
+            <Route path = '/SignUp/' elenment={<SignUp />} />
+        </main>
+    </BrowserRouter>
   );
-}
+};
 
 export default App;
