e43e829d885585a5e28c13e58718f2ba1d2189bd
jjhoneyy <jjhoneyyyy@gmail.com>
2024-02-27 17:21:56 +0900

    SignUp) 비밀번호 조건 설정

--- src/Pages/Signup.jsx
+++ src/Pages/Signup.jsx
@@ -1,17 +1,8 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import styled from "styled-components";
 import { useForm } from "react-hook-form";
 
-// 참고자료
-// https://velog.io/@devfrank9/React7-%EB%A6%AC%EC%95%A1%ED%8A%B8-%ED%8F%BC-%EC%9C%A0%ED%9A%A8%EC%84%B1%EA%B2%80%EC%82%AC-%EA%B9%94%EB%81%94%ED%95%98%EA%B2%8C-%EC%8D%A8%EB%B3%B4%EA%B8%B0-ft.-%ED%9A%8C%EC%9B%90%EA%B0%80%EC%9E%85-%EB%A1%9C%EA%B7%B8%EC%9D%B8
 
-// React Hook Form
-// https://jjang-j.tistory.com/47
-// https://cocococo.tistory.com/entry/React-React-Hook-Form
-
-// 비밀번호 일치 유효성 체크h
-// https://velog.io/@h_jinny/React-hook-form-%EB%B9%84%EB%B0%80%EB%B2%88%ED%98%B8-%EC%9D%BC%EC%B9%98-%EC%9C%A0%ED%9A%A8%EC%84%B1%EC%B2%B4%ED%81%AC
 
 const Container = styled.div`
   font-family: "Noto Sans KR";
@@ -105,7 +96,7 @@ const Signup = () => {
               type="email"
               {...register("email", {
                 required: true,
-                pattern: /^[A-Za-z0-9_\.\-]+@[A-Za-z0-9\-]+\.[A-za-z0-9\-]+/,
+                pattern: /^[A-Za-z0-9_.-]+@[A-Za-z0-9-]+\.[A-za-z0-9-]+/,
               })}
               errors={errors}
               placeholder="ssafe11@gmail.com"
@@ -119,7 +110,7 @@ const Signup = () => {
                 required: "비밀번호는 필수 입력입니다",
                 minLength: 8,
                 maxLength: 16,
-                // 비밀번호는 8자 이상 16자 이하의 문자열로 하나 이상의 영문자와 하나 이상의 숫자, 하나 이상의 특수문자로 구성되어야합니다.
+                pattern: /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@#$%^&*])[A-Za-z\d@#$%^&*]{8,16}$/
               })}
               errors={errors}
               placeholder="********"
@@ -131,6 +122,7 @@ const Signup = () => {
               type="password"
               {...register("passwordCheck", {
                 required: "비밀번호 확인이 필요합니다",
+                pattern: /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@#$%^&*])[A-Za-z\d@#$%^&*]{8,16}$/
               })}
               placeholder="********"
             />
